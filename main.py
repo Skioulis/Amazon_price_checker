f7f65f7e9bf4c7fcdeef1e1f4780d96fd249c2ba
Fotis Fotiadis <skioulis@gmail.com>
2024-10-25 17:15:54 +0300

    Checking the price if it's lower and sent me an email

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ import sent_email as se
 practice_url = "https://appbrewery.github.io/instant_pot/"
 live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
+# Scraping
 response = requests.get(practice_url)
 # response = requests.get(live_url)
 response.raise_for_status()
-
 static_site = response.text
 
 soup = BeautifulSoup(static_site, 'html.parser')
 
-result = soup.find(name="span", class_="aok-offscreen")
+# getting the curent price
+result_price = soup.find(name="span", class_="aok-offscreen")
+scrapped_price = float(result_price.getText().replace('$', ''))
+item = soup.find(id="productTitle").get_text().strip(' ').replace("  ", '').split('\n')[0]
 
-scrapped_price = float(result.getText().replace('$', ''))
+target_price= 100
 
-se.sent_email()
+if scrapped_price <target_price:
+    se.sent_email(f"{item} is below to target price: {target_price}$ at {scrapped_price}$")
+    print("its lower") # TODO: Delete
+else: print("not lower") # TODO: Delete
+
+
+
+
--- sent_email.py
+++ sent_email.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 import os
 
 
-def sent_email():
-
+def sent_email(message: str):
+    subject = "Amazon Price Alert!"
     email_to = "skioulis@gmail.com"
     load_dotenv(dotenv_path="ENV files/.env")
     smtp = os.getenv("GMAIL_smtp")
@@ -14,7 +14,8 @@ def sent_email():
     connection = smtplib.SMTP(smtp, port=port)
     connection.starttls()
     connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs=email_to, msg="test from env")
+    connection.sendmail(from_addr=my_email, to_addrs=email_to, msg=f"Subject:{subject}\n\n{message}".encode("utf-8"))
     connection.close()
-    print ("ok")
+
+    print ("ok") # TODO: delete
 
